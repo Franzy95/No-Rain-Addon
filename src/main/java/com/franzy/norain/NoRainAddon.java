47b43b96fd66bf154b66e44430c1fb0afa83462a
Franzy95 <fran.jemic95@gmail.com>
2026-05-13 23:39:17 +0200

    Update to 3.1.0

--- src/main/java/com/franzy/norain/NoRainAddon.java
+++ src/main/java/com/franzy/norain/NoRainAddon.java
@@ -12,41 +12,97 @@ public class NoRainAddon extends BTWAddon {
 
     public static int disableRainTicks = 240000;
 
-    public NoRainAddon() {
-    }
+    public static boolean disableRainForever = true;
 
     @Override
     public void initialize() {
+
         loadConfig();
-        System.out.println("[No Rain Addon] Initialized! disableRainTicks = " + disableRainTicks);
+
+        System.out.println(
+                "[No Rain Addon] Initialized!"
+        );
+
+        System.out.println(
+                "disableRainTicks = " + disableRainTicks
+        );
+
+        System.out.println(
+                "disableRainForever = " + disableRainForever
+        );
     }
 
     private void loadConfig() {
+
         try {
+
             File configDir = new File("config");
+
             if (!configDir.exists()) {
                 configDir.mkdirs();
             }
 
-            File configFile = new File(configDir, "norain.cfg");
+            File configFile =
+                    new File(configDir, "norain.conf");
+
             Properties props = new Properties();
 
             if (configFile.exists()) {
-                FileInputStream in = new FileInputStream(configFile);
+
+                FileInputStream in =
+                        new FileInputStream(configFile);
+
                 props.load(in);
+
                 in.close();
 
-                String ticksStr = props.getProperty("disableRainTicks", "240000");
-                disableRainTicks = Integer.parseInt(ticksStr);
+                String ticksStr =
+                        props.getProperty(
+                                "disableRainTicks",
+                                "240000"
+                        );
+
+                disableRainTicks =
+                        Integer.parseInt(ticksStr);
+
+                String foreverStr =
+                        props.getProperty(
+                                "disableRainForever",
+                                "true"
+                        );
+
+                disableRainForever =
+                        Boolean.parseBoolean(foreverStr);
+
             } else {
-                props.setProperty("disableRainTicks", String.valueOf(disableRainTicks));
-                FileWriter writer = new FileWriter(configFile);
-                props.store(writer, "NoRainAddon Configuration");
+
+                props.setProperty(
+                        "disableRainTicks",
+                        String.valueOf(disableRainTicks)
+                );
+
+                props.setProperty(
+                        "disableRainForever",
+                        String.valueOf(disableRainForever)
+                );
+
+                FileWriter writer =
+                        new FileWriter(configFile);
+
+                props.store(
+                        writer,
+                        "NoRainAddon Config"
+                );
+
                 writer.close();
             }
 
         } catch (IOException e) {
-            System.err.println("[No Rain Addon] Failed to load config: " + e.getMessage());
+
+            System.err.println(
+                    "[No Rain Addon] Failed to load config: "
+                            + e.getMessage()
+            );
         }
     }
 }
--- src/main/java/com/franzy/norain/mixin/WorldMixin.java
+++ src/main/java/com/franzy/norain/mixin/WorldMixin.java
@@ -1,9 +1,10 @@
 package com.franzy.norain.mixin;
 
-
 import com.franzy.norain.NoRainAddon;
+
 import net.minecraft.src.World;
 import net.minecraft.src.WorldInfo;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,16 +13,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(World.class)
 public class WorldMixin {
 
-    @Inject(method = "updateWeather", at = @At("HEAD"))
+    @Inject(
+            method = "updateWeather",
+            at = @At("HEAD")
+    )
     private void onUpdateWeather(CallbackInfo ci) {
+
         World world = (World)(Object)this;
+
         WorldInfo info = world.getWorldInfo();
 
         long time = world.getWorldTime();
 
-        if (time < NoRainAddon.disableRainTicks) {
-            world.getWorldInfo().setRaining(false);
-            world.getWorldInfo().setThundering(false);
+        if (
+                NoRainAddon.disableRainForever
+                        ||
+                        time < NoRainAddon.disableRainTicks
+        ) {
+
+            info.setRaining(false);
+
+            info.setThundering(false);
         }
     }
 }
